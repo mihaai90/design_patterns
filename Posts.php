083dbba24eed3a68b72e615fe90d61b663b9e1a9
mihaai90 <mihaai90@gmail.com>
2017-08-25 23:35:39 +0300

    Add files via upload

---
+++ Posts.php
@@ -0,0 +1,51 @@
+<?php
+class Posts{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $slug;
+    /**
+     * @var string
+     */
+    protected $title;
+    /**
+     * @var string
+     */
+    protected $content;
+    /**
+     * @var string
+     */
+    protected $create_date;
+    /**
+     * @var string
+     */
+    protected $author;
+    /**
+     * @var int
+     */
+    protected $status;
+
+    /**
+     * @param none
+     * @return none
+     */
+    public function __construct(){
+
+    }
+
+
+    public function find($id=0){}
+
+
+    public function save(){}
+
+
+    public function delete(){}
+
+
+
+}
